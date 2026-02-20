58f41f9bc60e6506b8f93ba31e33b5ad1f91d91e
Erika Gomez <eygomez@unbosque.edu.co>
2026-02-19 23:17:26 -0500

    Fix ruta imagen public

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://eri-02.github.io',
+  base: '/e-portafolio/',
 });
